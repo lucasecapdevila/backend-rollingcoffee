02a269bda388800a86d1a7b341b49e3c17257ada
Lucas Capdevila <lcapdevila60@gmail.com>
2024-03-05 18:35:29 -0300

    configuramos middlewares y configuramos las rutas

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import express from 'express'
+import 'dotenv/config'  //  Permite procesar variables de entorno (.env)
+import cors from 'cors'
+import morgan from 'morgan'
 
 //  1) Configurar un puerto
 const app = express()   //  Generalmente se llama app, pero puede llevar cualquier nombre
@@ -8,5 +11,14 @@ app.listen(app.get('port'), () => {
 })
 
 //  2) Configurar los middlewares
+app.use(cors())    //  Permite obtener conexiones remotas
+app.use(morgan('dev'))   //  Nos da info extra en la terminal
+//  Agregar middlewares faltantes
 
 //  3) Configurar las rutas
+//  http://localhost:3000/productos   la usabamos en json-server
+app.get('/', (req, res) => {
+  //  Agregar lógica que debería hacer el Backend
+  console.log('Procesando la solicitud Get');
+  res.send('Respuesta del backend RollingCoffee')
+})
