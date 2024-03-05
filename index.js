1968d1e75753e181456bc357814cc5b9e1434210
Lucas Capdevila <lcapdevila60@gmail.com>
2024-03-05 17:24:42 -0300

    instalamos express y habilitamos los módulos

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+import express from 'express'
+
 console.log('Consoleado desde index.js');
+console.log('Consoleado desde index.js v2');
+
+//  1) Configurar un puerto
+
+//  2) Configurar los middlewares
+
+//  3) Configurar las rutas
