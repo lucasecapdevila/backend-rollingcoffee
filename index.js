1c251a7a3c662ef0f876a6fd0e4c8b97f81b64f9
Lucas Capdevila <lcapdevila60@gmail.com>
2024-03-05 17:59:02 -0300

    agregamos Nodemon para usar el repo en desarrollo

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from 'express'
 
 console.log('Consoleado desde index.js');
-console.log('Consoleado desde index.js v2');
+console.log('Consoleado desde index.js v2.1');
+console.log('Consoleado desde index.js v2.2');
+console.log('Consoleado desde index.js v2.3');
 
 //  1) Configurar un puerto
 
