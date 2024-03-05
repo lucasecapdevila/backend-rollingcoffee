c1fe7a1ee0bb2052bb3b4756f056b30aea3ad50d
Lucas Capdevila <lcapdevila60@gmail.com>
2024-03-05 18:10:33 -0300

    Configuramos un puerto para el proyecto de Backend

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import express from 'express'
 
-console.log('Consoleado desde index.js');
-console.log('Consoleado desde index.js v2.1');
-console.log('Consoleado desde index.js v2.2');
-console.log('Consoleado desde index.js v2.3');
-
 //  1) Configurar un puerto
+const app = express()   //  Generalmente se llama app, pero puede llevar cualquier nombre
+app.set('port', process.env.PORT || 4000)
+app.listen(app.get('port'), () => {
+  console.log('Consoleado desde el puerto ' + app.get('port'));
+})
 
 //  2) Configurar los middlewares
 
