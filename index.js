8eb840832e2b9a2ae4f0ab8b740bc616991c7f83
Lucas Capdevila <lcapdevila60@gmail.com>
2024-03-07 17:01:54 -0300

    configuramos un archivo estático en la ruta principal del backend

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from 'express'
 import 'dotenv/config'  //  Permite procesar variables de entorno (.env)
 import cors from 'cors'
 import morgan from 'morgan'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
 //  1) Configurar un puerto
 const app = express()   //  Generalmente se llama app, pero puede llevar cualquier nombre
@@ -13,11 +15,21 @@ app.listen(app.get('port'), () => {
 //  2) Configurar los middlewares
 app.use(cors())    //  Permite obtener conexiones remotas
 app.use(morgan('dev'))   //  Nos da info extra en la terminal
-//  Agregar middlewares faltantes
+app.use(express.json())   //  Permite interpretar los datos en formato .json
+app.use(express.urlencoded({extended: true}))   //  Interpreta datos del body del request
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+// console.log(__filename);
+// console.log(path.join(__dirname, '/public'));
+
+
+app.use(express.static(path.join(__dirname,'/public')))
 
 //  3) Configurar las rutas
-//  http://localhost:3000/productos   la usabamos en json-server
-app.get('/', (req, res) => {
+//  http://localhost:3000/productos   => la usabamos en json-server
+//  http://localhost:4001/   => la usamos en Postman
+app.get('/nuevo', (req, res) => {
   //  Agregar lógica que debería hacer el Backend
   console.log('Procesando la solicitud Get');
   res.send('Respuesta del backend RollingCoffee')
