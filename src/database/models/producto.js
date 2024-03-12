5bb1235428c7a44fce9b64ef39407029b2f50252
Lucas Capdevila <lcapdevila60@gmail.com>
2024-03-12 17:31:47 -0300

    creamos el esquema del producto

---
+++ src/database/models/producto.js
@@ -0,0 +1,45 @@
+import mongoose, {Schema} from "mongoose";
+
+const productoSchema = new Schema({
+  nombreProducto: {
+    type: String,
+    required: true,
+    minLength: 2,
+    maxLength: 50,
+    unique: true
+  },
+  precio: {
+    type: Number,
+    required: true,
+    min: 50,
+    max: 10000
+  },
+  imagen: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (valor)=> {
+        const pattern = /(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|jpeg|gif|png)/
+        return pattern.test(valor)
+      },
+      message: dato => `${dato.value} no es una URL de imágen válida.`
+    }
+  },
+  categoria: {
+    type: String,
+    required: true,
+    enum: ['Infusiones', 'Batidos', 'Dulce', 'Salado']
+  },
+  descripcionBreve: {
+    type: String,
+    required: true,
+    minLength: 10,
+    maxLength: 80
+  },
+  descripcionAmplia: {
+    type: String,
+    required: true,
+    minLength: 20,
+    maxLength: 500
+  }
+})
