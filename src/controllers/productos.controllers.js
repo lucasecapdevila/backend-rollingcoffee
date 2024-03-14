6edd090e5cf89761ec4df5ae4dc83f8928a418b7
Lucas Capdevila <lcapdevila60@gmail.com>
2024-03-14 16:03:08 -0300

    se agrega lógica para crear un producto

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,4 +1,27 @@
+import Producto from "../database/models/producto.js";
+
 export const listarProductos = (req, res) => {
   console.log('Aquí preparo la lista de productos');
   res.send('Enviando la lista de productos')
+}
+
+export const crearProducto = async(req, res) => {
+  try {
+    //  Extraer los datos del body
+    console.log(req);
+    console.log(req.body);
+    //  todo: validar los datos del body
+    //  Pedir a la DB crear el producto
+    const productoNuevo = new Producto(req.body)
+    await productoNuevo.save()
+    //  Responder al frontend (éxito o error)
+    res.status(201).json({
+      mensaje: 'El producto fue creado exitosamente.'
+    })
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({
+      mensaje: 'El producto no pudo ser creado'
+    })
+  }
 }
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { listarProductos } from "../controllers/productos.controllers.js";
+import { crearProducto, listarProductos } from "../controllers/productos.controllers.js";
 
 const productosRouter = Router()
 
 //  Como crear las rutas
-productosRouter.route('/productos').get(listarProductos)
+productosRouter.route('/productos').get(listarProductos).post(crearProducto)
 
 export default productosRouter
