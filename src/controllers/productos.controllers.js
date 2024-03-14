071570da058be59ca954e608a792136aa46e5885
Lucas Capdevila <lcapdevila60@gmail.com>
2024-03-14 16:52:48 -0300

    agregamos la lógica de listarProductos para devolver el array de productos

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,8 +1,13 @@
 import Producto from "../database/models/producto.js";
 
-export const listarProductos = (req, res) => {
-  console.log('Aquí preparo la lista de productos');
-  res.send('Enviando la lista de productos')
+export const listarProductos = async(req, res) => {
+  try {
+    const listaProductos = await Producto.find()
+    res.status(200).json(listaProductos)
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({mensaje: 'Error al buscar los productos'})
+  }
 }
 
 export const crearProducto = async(req, res) => {
